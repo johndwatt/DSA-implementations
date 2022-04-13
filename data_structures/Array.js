848ee28523afaf031cb92c57a22d1fd6b8eec541
johndwatt <jdw23@protonmail.com>
2022-04-13 14:37:52 -0700

    instantiates array and adds get method

--- data_structures/Array.js
+++ data_structures/Array.js
@@ -12,4 +12,21 @@
     // *Push/append can sometimes be O(n)
 
 // IMPLEMENTATION:
-    
+
+class ArrayImp {
+    constructor() {
+        this.length = 0;
+        this.data = {};
+    }
+
+    /**
+     * Get value at array index.
+     * @param {Number} idx Index of array where value is stored.
+     * @returns Value stored at index.
+     */
+    get(idx) {
+        return this.data[idx];
+    }
+}
+
+const myArray = new ArrayImp();
