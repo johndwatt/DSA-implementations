ac625e886dcd074f4fdd82995e831179c9c54623
johndwatt <jdw23@protonmail.com>
2022-04-13 14:28:27 -0700

    adds array file and initial info

---
+++ data_structures/Array.js
@@ -0,0 +1,15 @@
+// === ARRAY ===
+
+// Definition: Ordered list of items stored sequentially in memory.
+
+// Big O:
+    // Lookup: O(1)
+    // Push: O(1)
+    // Insert: O(n)
+    // Delete: O(n)
+    // Search: O(n)
+
+    // *Push/append can sometimes be O(n)
+
+// IMPLEMENTATION:
+    
