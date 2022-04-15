9ed59ed32ea0f3bff4056b9518116529ed23c758
johndwatt <jdw23@protonmail.com>
2022-04-14 18:47:01 -0700

    adds linked list file and info to DS

---
+++ data_structures/Linked_List.js
@@ -0,0 +1,14 @@
+// === Linked List ===
+
+// Definition: A series of nodes that contain stored values and a pointer that points to the next node in the list.
+
+// Singly vs Doubly Linked List: Doubly linked lists also contain a prev property that points to the previous node in the list. 
+
+// Big O:
+    // Lookup: O(n)
+    // Prepend: O(1)
+    // Append: O(1)
+    // Insert: O(n)
+    // Delete: O(n)
+
+// IMPLEMENTATION:
