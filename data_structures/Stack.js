21f76d25839b33e8f5af02420c854a1b218ee851
johndwatt <jdw23@protonmail.com>
2022-04-14 17:41:30 -0700

    adds push method to stack class

--- data_structures/Stack.js
+++ data_structures/Stack.js
@@ -9,18 +9,33 @@
     // Peek: O(1)
 
 // IMPLEMENTATION - Array:
-
 class MyStack {
     constructor() {
         this.array = []
     }
 
     /**
-     * Returns item at the top of the stack.
+     * Retrieves value of item at the top of the stack.
+     * @returns Value at the top of the stack.
      */
     peek() {
         return this.array[this.array.length - 1];
     }
+
+    /**
+     * Adds an item to the top of the stack. 
+     * @param {*} item Value to be added to the top of the stack.
+     * @returns Updated stack. 
+     */
+    push(item) {
+        this.array.push(item);
+        return this;
+    }
 }
 
 const stackTest = new MyStack();
+console.log(stackTest.push("Bird"));
+console.log(stackTest.push("Plane"));
+console.log(stackTest.peek());
+
+
