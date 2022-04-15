ca672abc9f4e2b20960665d19285772343e42bb3
johndwatt <jdw23@protonmail.com>
2022-04-14 18:11:53 -0700

    adds queue DS file

---
+++ data_structures/Queue.js
@@ -0,0 +1,11 @@
+// === STACK ===
+
+// Definition: A first in first out data structure where only the earliest added item can be easily accessed or changed.
+
+// Big O:
+    // Lookup: O(n)
+    // Enqueue: O(1)
+    // Dequeue: O(1)
+    // Peek: O(1)
+
+// IMPLEMENTATION:
