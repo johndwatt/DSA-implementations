e6eacade1aba1226a83e628d768d2fd7d6cfc379
johndwatt <jdw23@protonmail.com>
2022-04-19 17:02:21 -0700

    adds remove method to binary search tree

--- data_structures/Binary_Search_Tree.js
+++ data_structures/Binary_Search_Tree.js
@@ -96,17 +96,88 @@ class BinarySearchTree {
         return null;
     }
 
+    /**
+     * Locate and remove node where given value is stored.
+     * @param {*} value Value to be removed.
+     * @returns Boolean indicating if removal was a success.
+     */
+    remove(value) {
+        if (!this.root) {
+            return false;
+        }
+        let currentNode = this.root;
+        let parentNode = null;
+        while (currentNode) {
+            if (value < currentNode.value) {
+                parentNode = currentNode;
+                currentNode = currentNode.left;
+            } else if (value > currentNode.value) {
+                parentNode = currentNode;
+                currentNode = currentNode.right;
+            } else if (value === currentNode.value) {
+                // No right child:
+                if (currentNode.right === null) {
+                    if (parentNode === null) {
+                        this.root = currentNode.left;
+                    } else {
+                        if (currentNode.value < parentNode.value) {
+                            parentNode.left = currentNode.left;
+                        } else if (currentNode.value > parentNode.value) {
+                            parentNode.right = currentNode.left;
+                        }
+                    }
+                // Right child with no left child:
+                } else if (currentNode.right.left === null) {
+                    currentNode.right.left === currentNode.left;
+                    if (parentNode === null) {
+                        this.root = currentNode.right;
+                    } else {
+                        if (currentNode.value < parentNode.value) {
+                            parentNode.left = currentNode.right;
+                        } else if (currentNode.value > parentNode.value) {
+                            parentNode.right = currentNode.right;
+                        }
+                    }
+                // Right child with a left child:
+                } else {
+                    let leftmost = currentNode.right.left;
+                    let leftmostParent = currentNode.right;
+                    while (leftmost.left !== null) {
+                        leftmostParent = leftmost;
+                        leftmost = leftmost.left;
+                    }
+
+                    leftmostParent.left = leftmost.right;
+                    leftmost.left = currentNode.left;
+                    leftmost.right = currentNode.right;
+
+                    if (parentNode === null) {
+                        this.root = leftmost;
+                    } else {
+                        if (currentNode.value < parentNode.value) {
+                            parentNode.left = leftmost;
+                        } else if (currentNode.value > parentNode.value) {
+                            parentNode.right = leftmost;
+                        }
+                    }
+                }
+                return true;
+            }
+        }
+    }
 }
 
 const tree = new BinarySearchTree();
 console.log(tree);
-console.log(tree.insert(10))
-console.log(tree.insert(5))
-console.log(tree.insert(50))
-console.log(traverse(tree.root))
-console.log(tree.insert(1))
-console.log(tree.insert(55))
-console.log(tree.insert(5000))
-console.log(traverse(tree.root))
-console.log(tree.lookup(55))
+console.log(tree.insert(10));
+console.log(tree.insert(5));
+console.log(tree.insert(50));
+console.log(traverse(tree.root));
+console.log(tree.insert(1));
+console.log(tree.insert(55));
+console.log(tree.insert(5000));
+console.log(traverse(tree.root));
+console.log(tree.lookup(55));
+console.log(tree.remove(55));
+console.log(tree.lookup(55));
 
