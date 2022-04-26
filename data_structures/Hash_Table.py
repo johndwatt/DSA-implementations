a61015c482c8ad8509a04c5c63ee28c64c7e9dda
johndwatt <jdw23@protonmail.com>
2022-04-26 13:48:52 -0700

    creates python hash table file

---
+++ data_structures/Hash_Table.py
@@ -0,0 +1,12 @@
+# === ARRAY ===
+
+# Definition: Ordered list of items stored sequentially in memory.
+
+# Big O:
+    # Lookup: O(1)
+    # Push: O(1)
+    # Insert: O(n)
+    # Delete: O(n)
+    # Search: O(n)
+
+# IMPLEMENTATION:
