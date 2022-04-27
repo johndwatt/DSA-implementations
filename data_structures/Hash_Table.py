9a1fe25fdaf4a6845a77a80421db7e6eac87d7b4
johndwatt <jdw23@protonmail.com>
2022-04-27 00:30:03 -0700

    adds hash and instantiates table in file

--- data_structures/Hash_Table.py
+++ data_structures/Hash_Table.py
@@ -10,3 +10,16 @@
     # Search: O(n)
 
 # IMPLEMENTATION:
+class Hash_Table:
+    def __init__(self) -> None:
+        self.data = []
+    
+    def _hash(self, key):
+        """Returns a memory location for the given key."""
+        hash = 0
+        for i in range(len(key)):
+            hash = (hash + key[i] * i) % len(self.data)
+        return hash
+    
+
+
