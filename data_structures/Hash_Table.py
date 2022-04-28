9d1e02e26fa5f1daf9eaaed91d0587e38c1ac331
johndwatt <jdw23@protonmail.com>
2022-04-27 18:04:05 -0700

    adds set method to py hash table and fixes errors with hash function

--- data_structures/Hash_Table.py
+++ data_structures/Hash_Table.py
@@ -11,15 +11,30 @@
 
 # IMPLEMENTATION:
 class Hash_Table:
-    def __init__(self) -> None:
-        self.data = []
+    def __init__(self, size):
+        self.size = size
+        self.data = [None]*self.size
+
+    def __str__(self):
+        """Prints the attributes of the class object in a dictionary format"""
+        return str(self.__dict__)
     
-    def _hash(self, key):
+    def _hash(self, key: str):
         """Returns a memory location for the given key."""
         hash = 0
         for i in range(len(key)):
-            hash = (hash + key[i] * i) % len(self.data)
+            # ord(key[i]) gives the unicode code point of the character key[i]
+            hash = (hash + ord(key[i]) * i) % len(self.data)
         return hash
     
+    def set(self, key: str, value):
+        """Adds a key value pair at the hashed address."""
+        address = self._hash(key)
+        if not self.data[address]:
+            self.data[address] = []
+        self.data[address].append([key, value])
+        return self.data
 
 
+my_map = Hash_Table(10)
+print(my_map.set("1", "Brian"))
