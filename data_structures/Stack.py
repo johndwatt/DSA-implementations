5a54e8441f5bf484c0c75cf3b962a6f2b49d1d55
johndwatt <jdw23@protonmail.com>
2022-05-25 19:24:00 -0700

    adds stack array implementation

--- data_structures/Stack.py
+++ data_structures/Stack.py
@@ -14,8 +14,32 @@ class MyStack:
         self.stack = []
     
     def __str__(self):
-        """Prints the attributes of the class object in a dictionary format"""
+        """Prints the attributes of the class object in a dictionary format."""
         return str(self.__dict__)
+
+    def peek(self):
+        """Retrieves value of item at the top of the stack."""
+        return self.stack[-1]
+
+    def push(self, item):
+        """Adds an item to the top of the stack."""
+        self.stack.append(item)
+        return self.stack
+
+    def pop(self):
+        """Removes an item from the top of the stack."""
+        self.stack.pop()
+        return self.stack
     
-stack_test = MyStack()
-print(stack_test)
+# stack_test = MyStack()
+# print(stack_test)
+# print(stack_test.push("bird"))
+# print(stack_test.push("plane"))
+# print(stack_test.peek())
+# print(stack_test.push("delete me"))
+# print(stack_test)
+# stack_test.pop()
+# print(stack_test)
+
+
+
