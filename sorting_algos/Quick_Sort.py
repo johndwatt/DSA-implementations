fc4644c4c01f69a8c093fe8a72280685e2ae1dc3
johndwatt <jdw23@protonmail.com>
2022-05-30 18:14:42 -0700

    adds python quick sort implementation

--- sorting_algos/Quick_Sort.py
+++ sorting_algos/Quick_Sort.py
@@ -11,3 +11,31 @@
 
 # // IMPLEMENTATION:
 nums = [10, 295, 37, 2, 84, 50, 1, 7, 86, 310, 462, 19, 0]
+
+def quick_sort (array, left, right):
+    if len(array) == 1:
+        return array
+    if left < right:
+        partition_index = partition(array, left, right)
+        quick_sort(array, left, partition_index - 1)
+        quick_sort(array, partition_index + 1, right)
+    return array
+
+def partition(array, left, right):
+    pivot = array[right]
+    partition_index = left
+    for i in range(left, right):
+        if array[i] <= pivot:
+            swap(array, i, partition_index)
+            partition_index += 1
+    swap(array, right, partition_index)
+    return partition_index
+
+def swap(array, first_idx, second_idx):
+    temp = array[first_idx]
+    array[first_idx] = array[second_idx]
+    array[second_idx] = temp
+
+# Select first and last index as 2nd and 3rd parameters
+print(quick_sort(nums, 0, len(nums) - 1))
+
